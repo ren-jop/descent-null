1943688c4cc73a7f57400b90ba3486e8b782ef55
Ren Jopson <rin674167@gmail.com>
2026-09-17 01:11:05 +1000

    Export session leaderboard records only

--- src/world/mod.rs
+++ src/world/mod.rs
@@ -4,4 +4,4 @@ mod cave;
 mod records;
 
 pub use cave::{CavePlugin, CurrentDepth, DepthAnnouncement, RunStats};
-pub use records::{BestTimes, RecordsPlugin, SessionTimes};
+pub use records::{RecordsPlugin, RunRecord, SessionTimes};
