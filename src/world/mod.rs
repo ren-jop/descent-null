febb8cd8aeab981ec81e790f341c542d770ac34a
Ren Jopson <rin674167@gmail.com>
2026-09-16 23:36:02 +1000

    Export persistent best-time records

--- src/world/mod.rs
+++ src/world/mod.rs
@@ -1,5 +1,7 @@
 //! the game world: a procedurally generated, depth-layered cave.
 
 mod cave;
+mod records;
 
 pub use cave::{CavePlugin, CurrentDepth, DepthAnnouncement, RunStats};
+pub use records::{BestTimes, RecordsPlugin};
